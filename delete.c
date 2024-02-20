50cfc16a07ecedea7602163cfc1e2d96d97a5750
sid403 <160594733+sid403@users.noreply.github.com>
2024-02-20 16:14:30 +0530

    delete.c

---
+++ delete.c
@@ -0,0 +1,16 @@
+void deleteNode(struct ListNode* node) { struct
+ListNode *prev = node , *curr = node->next; while(curr != NULL)
+{
+int temp = prev->val; prev->val = curr-
+>val; curr->val = temp; if(prev->next->next
+== NULL)
+{
+prev->next = NULL; curr = curr-
+>next;
+} else
+{
+prev = prev->next; curr = curr-
+>next;
+}
+}
+}
